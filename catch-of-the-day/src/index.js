4e2a2ea62ede0a417d609a127685c4672e4d5240
Ravigopal Kesari <kesariravigopal@gmail.com>
2016-10-30 12:00:48 +0530

    Create application layout using components

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,7 +1,8 @@
 // let's go!
 import React from 'react';
 import { render } from 'react-dom';
+import App from './components/App';
 import StorePicker from './components/StorePicker';
 import './css/style.css';
 
-render(<StorePicker/>, document.querySelector('#main'));
+render(<App />, document.querySelector('#main'));
