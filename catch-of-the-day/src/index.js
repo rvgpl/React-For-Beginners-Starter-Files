37d6af4fe23918201da4fbbb42679571fc262188
Ravigopal Kesari <kesariravigopal@gmail.com>
2016-10-30 12:46:30 +0530

    Routing with React Router

--- catch-of-the-day/src/index.js
+++ catch-of-the-day/src/index.js
@@ -1,8 +1,21 @@
 // let's go!
 import React from 'react';
 import { render } from 'react-dom';
+import {BrowserRouter, Match, Miss } from 'react-router';
 import App from './components/App';
 import StorePicker from './components/StorePicker';
+import NotFound from './components/NotFound';
 import './css/style.css';
 
-render(<App />, document.querySelector('#main'));
+const Root = () => {
+  return (
+    <BrowserRouter>
+      <div>
+        <Match exactly pattern="/" component={StorePicker} />
+        <Match exactly pattern="/store/:storeId" component={App} />
+        <Miss component={NotFound} />
+      </div>
+    </BrowserRouter>
+  )
+}
+render(<Root />, document.querySelector('#main'));
