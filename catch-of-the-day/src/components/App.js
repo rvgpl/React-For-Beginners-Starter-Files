33cc767d312b256b7c7e3df7cc87d5786a89b14f
Ravigopal Kesari <kesariravigopal@gmail.com>
2016-11-09 21:15:25 +0530

    Persisting data using localstorage

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -44,11 +44,24 @@ class App extends React.Component {
         context: this,
         state: 'fishes'
       });
+
+    const localStorageRef = localStorage.getItem(`order-${this.props.params.storeId}`);
+    if(localStorageRef) {
+      this.setState({
+        order: JSON.parse(localStorageRef)
+      });
+    }
   }
 
   componentWillUnmount() {
     base.removeBinding(this.ref);
   }
+
+  componentWillUpdate(nextProps, nextState) {
+    localStorage.setItem(`order-${this.props.params.storeId}`, JSON.stringify(nextState.order));
+  }
+
+
 
   render() {
     return (
